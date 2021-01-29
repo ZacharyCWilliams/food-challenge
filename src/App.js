4c0113d17ca7236291255b9e2d7da1a0305ec54a
Zachary C Williams <ZachCWilliams@icloud.com>
2021-01-28 18:30:35 -0800

    clean up comments

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import Nav from "./Nav/Nav";
 import Banner from "./Banner/Banner";
 import MenuGrid from "./MenuGrid/MenuGrid";
 
-// Nav
-// Banner
-// <Menu>
-// H1
-// P
-// render <ItemGrid items={items} />
-// <Item />
-//
-//</Menu>
-
 const App = () => {
 
   return (
@@ -26,8 +16,3 @@ const App = () => {
 }
 
 export default App;
-
-/* 
-      <Banner />
-      <Menu />
-      <Footer /> */
